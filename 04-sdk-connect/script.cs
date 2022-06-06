51fb3521ee9c25cf50fea123d30e1d96366cf769
Postolaki Ruslan <97512521+amd-ruslan-postolaki@users.noreply.github.com>
2022-06-06 22:01:51 +0300

    Update script.cs
    sample from https://docs.microsoft.com/en-gb/learn/modules/use-azure-cosmos-db-sql-api-sdk/?WT.mc_id=cloudskillschallenge_0bdd75d2-7737-4c9c-bcb0-bfaf4cc8ec56&ns-enrollment-type=Collection&ns-enrollment-id=kkqrh6265geo

--- 04-sdk-connect/script.cs
+++ 04-sdk-connect/script.cs
@@ -1,1 +1,14 @@
-﻿
+using System;
+using System.Linq;
+
+using Microsoft.Azure.Cosmos;
+
+string endpoint = "<cosmos-endpoint>";
+string key = "<cosmos-key>";
+
+CosmosClient client = new (endpoint, key);
+
+AccountProperties account = await client.ReadAccountAsync();
+
+Console.WriteLine($"Account Name:\t{account.Id}");
+Console.WriteLine($"Primary Region:\t{account.WritableRegions.FirstOrDefault()?.Name}");
