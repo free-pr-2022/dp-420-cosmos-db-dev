6129b0a0c1e4551bb89d4ba2ca86359e6bd2562c
Ruslan.Postolaki <ruslan.postolaki@RuslanPostolakis-Mac-Pro.local>
2022-06-14 23:52:27 +0300

    Module 13. Define indexes in Azure Cosmos DB SQL API.md

--- 12-custom-index-policy/script.cs
+++ 12-custom-index-policy/script.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using Microsoft.Azure.Cosmos;
 
-string endpoint = "<cosmos-endpoint>";
+string endpoint = "https://vvv-ooo.documents.azure.com:443/";
 
-string key = "<cosmos-key>";
+string key = "jm0Qj3sTtK0wGl1F2aFKlpIOxanxZUyMcKRKBOuxT9c42xN0PUkoc5MltC3y9KPQE6nWEWJT6M8EYghDBQCgtQ==";
 
 CosmosClient client = new CosmosClient(endpoint, key);
 
 Database database = await client.CreateDatabaseIfNotExistsAsync("cosmicworks");
+
+
+IndexingPolicy policy = new ();
+
+policy.IndexingMode = IndexingMode.Consistent;
+
+policy.ExcludedPaths.Add(
+    new ExcludedPath{ Path = "/*" }
+);
+
+policy.IncludedPaths.Add(
+    new IncludedPath{ Path = "/name/?" }
+);
+
+ContainerProperties options = new ("products", "/categoryId");
+options.IndexingPolicy = policy;
+
+Container container = await database.CreateContainerIfNotExistsAsync(options);
+Console.WriteLine($"Container Created [{container.Id}]");
+
+
+
+
